d86f12c5deda76e771b69680951899eec869daf9
chylex <info@chylex.com>
2014-11-12 20:13:52 +0100

    Main stuffs

---
+++ main.js
@@ -0,0 +1,31 @@
+/*global define, $, brackets, document */
+
+define(function(require,exports,module){
+  'use strict';
+  
+  var AppInit = brackets.getModule("utils/AppInit");
+  var DocumentManager = brackets.getModule("document/DocumentManager");
+  var ProjectManager = brackets.getModule("project/ProjectManager");
+  var MainViewManager = brackets.getModule("view/MainViewManager");
+  
+  function updateWindowTitle(){
+    var title = "";
+    
+    var proj = ProjectManager.getProjectRoot();
+    var path = MainViewManager.getCurrentlyViewedPath(MainViewManager.ACTIVE_PANE);
+    
+    if (proj)title += "Project - "+proj._name;
+    
+    if (path){
+      title += proj ? " - " : "File - ";
+      title += ProjectManager.makeProjectRelativeIfPossible(MainViewManager.getCurrentlyViewedFile()._path);
+    }
+    
+    document.title = title == "" ? brackets.config.app_title : title;
+  };
+  
+  AppInit.htmlReady(function(){ updateWindowTitle(); });
+  $(DocumentManager).on("dirtyFlagChange",function(){ updateWindowTitle(); });
+  $(ProjectManager).on("projectOpen",function(){ updateWindowTitle(); }).on("projectRefresh",function(){ updateWindowTitle(); });
+  $(MainViewManager).on("currentFileChange",function(){ updateWindowTitle(); });
+});
